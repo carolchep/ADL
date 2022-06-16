475858e8cb782a3b88c59b26962a4f262d2c80fa
carolchep <carolchepsam@gmail.com>
2022-06-16 10:54:18 +0300

    adding deployed server link

--- src/api/AuthRequests.js
+++ src/api/AuthRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://git.heroku.com/igcl.git' });
 
 export const logIn= (formData)=> API.post('/auth/login',formData);
 
